725f8b56a2d5e661c56b3989d3838beb0459b0f3
FletcherPoole <69251998+FletcherPoole@users.noreply.github.com>
2020-08-06 17:08:13 +0100

    Version 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+	
+	if (message.content === 'massimo') {
+			let imageNumber = Math.floor(Math.random()* 98) + 1
+			message.channel.send ( {files: ["./images/" + imageNumber + ".jpg"]} )
+		}
+
+});
+
+client.login(process.env.token);
